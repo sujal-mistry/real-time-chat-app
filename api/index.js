447000af73d2b7e04752326b38610d8f2a104909
Sujal Mistry <sujal.mistry104@gmail.com>
2024-08-29 17:13:19 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -17,8 +17,17 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = require("socket.io")(server, { origins: "*:*" });
-
+const io = require("socket.io")(server, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
 app.use(express.static(publicPath));
 
 var users = new Users();
